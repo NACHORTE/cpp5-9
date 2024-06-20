cae73fcdd75cff8bd613c0797b1b1b980a03f321
NACHORTE <ignaciortegape@gmail.com>
2024-06-20 19:11:47 +0200

    wish me luck

--- C07/ex01/main.cpp
+++ C07/ex01/main.cpp
@@ -32,11 +32,6 @@ Test& Test::operator=(const Test& rhs) {
   return *this;
 }
 
-
-void Test::sayHello(Test& a) {
-	std::cout << "Hello!! I am " << a._age << " years old" << std::endl;
-}
-
 void	incr(int& n) {
 	++n;
 }
@@ -47,11 +42,7 @@ void print(int& n) {
 
 int main() {
 	int arr[4] = {1, 2, 3, 4};
-	int i;
 
-	std::cout << "=======================================================================" << std::endl;
-	std::cout << "Lets start with a simple test, array of 4 integers => foreach increment" << std::endl;
-	std::cout << "=======================================================================" << std::endl;
   iter(arr, 4, &print);
 	std::cout << std::endl;
 
@@ -60,20 +51,9 @@ int main() {
   iter(arr, 4, &print);
 	std::cout << std::endl;
 
-
-	std::cout << "=======================================================================" << std::endl;
-	std::cout << "Now lets do something that is also quite generic, pass a null pointer" << std::endl;
-	std::cout << "=======================================================================" << std::endl;
-
 	iter((int*)NULL, 100, &incr);
 
-	std::cout << "(no crash)" << std::endl;
+	std::cout << "I did not crash!" << std::endl;
 
-	std::cout << "=======================================================================" << std::endl;
-	std::cout << "Now, just for fun, lets create an array of instances of the Test class" << std::endl;
-	std::cout << "=======================================================================" << std::endl;
-
-	Test *t = new Test[5];
-	iter(t, 5, &(t[i].sayHello));
-	delete[] t;
+	return 0;
 }
